527102a4c9d026e140f62b3ebcdf1dedd67e81ca
ms36 <mstevenson36@hotmail.com>
2019-11-15 15:08:08 -0500

    new commit

---
+++ src/main/java/com/skillstorm/data/ConnectionFactory.java
@@ -0,0 +1,6 @@
+package com.skillstorm.data;
+
+public class ConnectionFactory {
+
+	//Connection
+}
---
+++ src/main/java/com/skillstorm/data/TimeSheetDAO.java
@@ -0,0 +1,8 @@
+package com.skillstorm.data;
+
+public class TimeSheetDAO {
+/*public User findByUserName(String username) {};
+public List<TimeSheet> findTimeSheetByUser(int id){};
+public TimeSheet findTimeSheetbyId(int id) {};
+public TimeSheet save(TimeSheet t) {};*/
+}
